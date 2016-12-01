e2751ff15d4d6b63f4965910ce1a4f6bfa502d61
kiellywelly <kiela.agustin@gmail.com>
2016-12-01 15:35:34 +0800

    fixed header problem

---
+++ functions.php
@@ -0,0 +1,32 @@
+<?php
+
+
+if (isset($_POST["newBuild"])) {
+    $url = "http://localhost:9999/users/".$_SESSION["username"]."/builds/new";
+    $response = Helper::requestPost($url, $_POST);
+    $response = json_decode($response);
+    if ($response->status == 200) {
+        Helper::redirect("http://localhost/pcparts/build.php?id=".$response->body);
+	}
+}
+
+if (isset($_POST["login"])) {
+    $url = "http://localhost:9999/login";
+    $response = Helper::requestPost($url, $_POST);
+    $response = json_decode($response);
+    echo $response->body;
+    if ($response->status == 200) {
+        $_SESSION["username"] = $response->body;
+        Helper::redirect("http://localhost/pcparts/home.php");
+    } else {
+        $lError = "Invalid Credentials";
+    }
+} elseif (isset($_GET["logout"])) {
+    // $response = Helper::requestGet("http://localhost:9999/logout");
+    session_unset();
+} elseif (isset($_GET["auth"])) {
+    $error = "Please Log in first.";
+}
+
+
+?>
